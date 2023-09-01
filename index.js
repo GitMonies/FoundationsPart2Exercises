470650da58fa2f88da1bb970aaeefe9f5ffed549
GitMonies <jarod.smelosky@yahoo.com>
2023-08-31 19:55:21 -0500

    First Exercise

---
+++ index.js
@@ -0,0 +1,23 @@
+
+//1. Prompt and Greeting function
+
+// let userName = prompt('What is your name?')
+
+// function Greeting() {
+// let message = "Hello, " + userName + "!"
+// alert(message)
+// return message
+// }
+
+//  Greeting()
+
+// 2. Multiply Function
+
+function multiplication() {
+let numberOne = 5;
+let numberTwo = 15;
+let multiply = numberOne * numberTwo;
+console.log(multiply)
+}
+
+multiplication()
